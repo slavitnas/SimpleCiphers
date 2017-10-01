8f2f31eb800c5afab7148162c63465b0285a3772
slavitnas <slavitnas@gmail.com>
2017-10-02 00:23:27 +0300

    Added porta cipher

---
+++ SimpleCiphers/Models/ArrayOperations.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCiphers.Models
+{
+    public static class ArrayOperations
+    {
+        // Содержится ли text в encAbc. Если да, то возвращаются индексы в encAbc
+        //   a  b     text = 1
+        // a 1  2     return x = 0, y = 0
+        // b 3  4     abc[0,0] = aa - расшифрованный
+        public static bool ContainsIn(string text, string[,] encAbc, out int x, out int y)
+        {
+            for (int i = 0; i < encAbc.GetLength(0); i++)
+            {
+                for (int j = 0; j < encAbc.GetLength(1); j++)
+                {
+                    if (text == encAbc[i, j])
+                    {
+                        x = i;
+                        y = j;
+                        return true;
+                    }
+                }
+            }
+            x = -1;
+            y = -1;
+            return false;
+        }
+
+        // Содержится ли text на измерениях массива encAbc
+        //   1 2     text = 11
+        // 1 a b     return x = 0, y = 0
+        // 2 c d     encAbc[0,0] = a - зашифрованный
+        public static bool ContainsOut(string text, string[,] encAbc, out int x, out int y)
+        {
+            for (int i = 0; i < encAbc.GetLength(0); i++)
+            {
+                for (int j = 0; j < encAbc.GetLength(1); j++)
+                {
+                    if (text == $"{i + 1}{j + 1}" && encAbc[i, j] != string.Empty)
+                    {
+                        x = i;
+                        y = j;
+                        return true;
+                    }
+                }
+            }
+            x = -1;
+            y = -1;
+            return false;
+        }
+    }
+}
---
+++ SimpleCiphers/Models/PortaCipher.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCiphers.Models
+{
+    public class PortaCipher : ICipher
+    {
+        public string Encrypt(string text, string key, string abc)
+        {
+            return Crypt(text, abc, true);
+        }
+
+        public string Decrypt(string text, string key, string abc)
+        {
+            return Crypt(text, abc, false);
+        }
+
+        public string[,] GetEncryptedAlphabet(string key, string abc)
+        {
+            int len = abc.Length;
+            string leadZeros = $"D{Math.Floor(Math.Log10(len * len) + 1)}";
+            var abcArr = new string[len, len];
+            int count = 1;
+
+            for (int i = 0; i < abcArr.GetLength(0); i++)
+            {
+                for (int j = 0; j < abcArr.GetLength(1); j++)
+                {
+                    abcArr[i, j] = (count++).ToString(leadZeros);
+                }
+            }
+            return abcArr;
+        }
+
+        public string[] GetRowAlphabet(string abc)
+        {
+            var arr = new string[abc.Length];
+            for (int i = 0; i < abc.Length; i++)
+            {
+                arr[i] = abc[i].ToString();
+            }
+            return arr;
+        }
+
+        public string[] GetColAlphabet(string abc)
+        {
+            return GetRowAlphabet(abc);
+        }
+
+        public string Crypt(string text, string abc, bool encrypt)
+        {
+            int len = abc.Length;
+            // число значущих цифр при шифровании
+            int count = (int) Math.Floor(Math.Log10(len * len) + 1);
+
+            if (encrypt)
+            {
+                string check = string.Join("", abc.Union(text));
+                if (check != abc)
+                {
+                    throw new ArgumentException("Текст содержит символы не из алфавита.");
+                }
+
+                if (text.Length % 2 != 0)
+                {
+                    throw new ArgumentException(
+                        "Текст содержит нечетное количество символов.\n" +
+                        "Добавьте или удалите один символ для шифрования.");
+                }
+            }
+            else
+            {
+                if (!text.All(char.IsDigit))
+                {
+                    throw new ArgumentException(
+                        "Шифрованный текст должен состоять только из цифр.");
+                }
+
+                int num = text.Length % count;
+                if (num != 0)
+                {
+                    throw new ArgumentException(
+                        $"Длина шифрованного текста не кратна {count}.\n" +
+                        $"Введено {num} из {count} чисел.");
+                }
+            }
+
+            text = text.ToLowerInvariant();
+            // шифрованный алфавит
+            var encAbc = GetEncryptedAlphabet(null, abc);
+            string result = "";
+
+            if (encrypt)
+                for (int i = 0; i < text.Length - 1; i += 2)
+                {
+                    int row = abc.IndexOf(text[i]);
+                    int col = abc.IndexOf(text[i + 1]);
+                    result += $"{encAbc[row, col]}";
+                }
+            else
+            {
+                for (int i = 0; i < text.Length - count + 2; i += count)
+                {
+                    string temp = text.Substring(i, count);
+                    if (ArrayOperations.ContainsIn(temp, encAbc, out var x, out var y))
+                    {
+                        result += $"{abc[x]}{abc[y]}";
+                    }
+                    else
+                    {
+                        throw new ArgumentException(
+                            $"Невозможно расшифровать {temp}.\n" +
+                            "Данной комбинации нет в шифрованном алфавите.");
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
