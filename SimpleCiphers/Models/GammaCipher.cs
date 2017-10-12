65708597592005b9030558e7cff317939c352bbf
slavitnas <slavitnas@gmail.com>
2017-10-12 16:43:39 +0300

    Added gamma cipher

---
+++ SimpleCiphers/Models/GammaCipher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCiphers.Models
+{
+    public class GammaCipher : ICipher
+    {
+        public string Encrypt(string text, string key, string abc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decrypt(string text, string key, string abc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[,] GetEncryptedAlphabet(string text, string key, string abc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetRowAlphabet(string key, string abc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetColAlphabet(string key, string abc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
