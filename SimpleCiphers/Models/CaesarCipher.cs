fdf79dba051eb46efc1636c5359de387f7c86e3b
slavitnas <slavitnas@gmail.com>
2017-10-13 01:47:04 +0300

    Optimized code

--- SimpleCiphers/Models/CaesarCipher.cs
+++ SimpleCiphers/Models/CaesarCipher.cs
@@ -1,81 +1,49 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace SimpleCiphers.Models
 {
-    // Шифр Цезаря
     public class CaesarCipher : ICipher
     {
-        public string Encrypt(string text, string key, string abc)
-        {
-            return Crypt(text, key, abc, true);
-        }
+        public string Encrypt(string text, string key, string abc) => Crypt(text, key, abc, true);
 
-        public string Decrypt(string text, string key, string abc)
-        {
-            return Crypt(text, key, abc, false);
-        }
+        public string Decrypt(string text, string key, string abc) => Crypt(text, key, abc, false);
 
         public string[,] GetEncryptedAlphabet(string text, string key, string abc)
         {
-            string encAbc = Crypt(abc, key, abc, true);
-            string[,] arr = new string[1, encAbc.Length];
-            for (int i = 0; i < encAbc.Length; i++)
-            {
-                arr[0, i] = encAbc[i].ToString();
-            }
-            return arr;
+            var encAbc = Crypt(abc, key, abc, true);
+            return ArrayOperations.Turn1DTo2D(encAbc);
         }
 
-        public string[] GetRowAlphabet(string key, string abc)
-        {
-            return null;
-        }
+        public string[] GetRowAlphabet(string key, string abc) => null;
 
-        public string[] GetColAlphabet(string key, string abc)
-        {
-            return abc.Select(x => $"{x}").ToArray();
-        }
+        public string[] GetColAlphabet(string key, string abc) => abc.Select(x => $"{x}").ToArray();
 
         public string Crypt(string text, string key, string abc, bool encrypt)
         {
-            if (string.IsNullOrEmpty(key))
-            {
-                throw new ArgumentException("Необходимо задать ключ для шифра Цезаря.");
-            }
+            Checker.KeyNull(key);
+            var intKey = Checker.GetKeyInt(key);
+            Checker.TextNull(text);
+            Checker.TextContain(text, abc);
 
-            bool check = int.TryParse(key, out var intKey);
-
-            if (!check)
-            {
-                throw new ArgumentException("Ключ для шифра Цезаря должен состоять " +
-                                            "только из целых чисел в пределах int.");
-            }
-
-            string checkText = string.Join("", abc.Union(text));
-            if (checkText != abc)
-            {
-                throw new ArgumentException("Текст содержит символы не из алфавита.");
-            }
-
-            // для исключения переполнения
+            // to avoid overflows
             intKey %= abc.Length;
 
-            // если дешифрование, то надо отнимать
             if (!encrypt)
+            {
                 intKey *= -1;
+            }
 
-            // иначе будет обращаться к недопустимому индексу
+            // otherwise invalid index
             if (intKey < 0)
             {
                 intKey += abc.Length;
             }
 
-            string result = "";
+            var result = "";
 
-            foreach (char ch in text)
+            foreach (var ch in text)
             {
-                for (int j = 0; j < abc.Length; j++)
+                for (var j = 0; j < abc.Length; j++)
                 {
                     if (ch == abc[j])
                     {
