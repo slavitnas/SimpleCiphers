d64e5db101727cde4ef9a45221f75376e908b237
slavitnas <slavitnas@gmail.com>
2017-10-13 01:56:32 +0300

    Added new exception

---
+++ SimpleCiphers/Models/CipherException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCiphers.Models
+{
+    [Serializable]
+    public class CipherException : Exception
+    {
+        public CipherException()
+        {
+        }
+
+        public CipherException(string message) : base(message)
+        {
+        }
+
+        public CipherException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected CipherException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
